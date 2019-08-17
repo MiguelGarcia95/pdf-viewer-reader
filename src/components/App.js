e9cc31a49dc5e7ab1bd2ac810d153f2376abd6b1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-16 23:51:11 -0500

    toggle thru pages

--- src/components/App.js
+++ src/components/App.js
@@ -73,39 +73,16 @@ class App extends React.Component {
         viewport: viewport
       }
       
-      page.render(renderCtx).promise.then(() => {
-        // page render false
-        this.setState({pageIsRendering: false});
-        if (this.state.pageNumIsPending !== null) {
-          this.renderPage(this.state.pageNumIsPending);
-          this.setState({pageNumIsPending: null});
-        }
-      })
+      await page.render(renderCtx).promise
+      // page render false
+      this.setState({pageIsRendering: false});
+      if (this.state.pageNumIsPending !== null) {
+        this.renderPage(this.state.pageNumIsPending);
+        this.setState({pageNumIsPending: null});
+      }
     } catch (error) {
       console.log(error)
     }
-
-    // this.state.pdfDoc.getPage(pageNum).then(page => {
-    //   // set scale
-    //   const viewport = page.getViewport({scale: this.state.scale})
-    //   canvas.height = viewport.height;
-    //   canvas.width = viewport.width;
-
-    //   const renderCtx = {
-    //     canvasContext: canvas.getContext('2d'),
-    //     viewport: viewport
-    //   }
-      
-    //   page.render(renderCtx).promise.then(() => {
-    //     // page render false
-    //     this.setState({pageIsRendering: false});
-
-    //     if (this.state.pageNumIsPending !== null) {
-    //       this.renderPage(this.state.pageNumIsPending);
-    //       this.setState({pageNumIsPending: null});
-    //     }
-    //   })
-    // })
   }
 
   render() {
