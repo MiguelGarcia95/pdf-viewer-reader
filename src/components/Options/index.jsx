a66317d51773f2356bc2a5ca8035c620587159c3
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-18 02:39:17 -0500

    go to page added

--- src/components/Options/index.jsx
+++ src/components/Options/index.jsx
@@ -7,18 +7,20 @@ function Options({previousPage, nextPage, loadPdf, url, uploadPdf, onPageSubmit}
   // update scale?
   // change colors?
   return (
-    <section>
-      <OnClickBtn className='btn sm' onBtnClick={() => previousPage()} btnText='' btnIcon='fas fa-lg fa-chevron-circle-left' />
-      <OnClickBtn className='btn sm' onBtnClick={() => nextPage()} btnText='' btnIcon='fas fa-lg fa-chevron-circle-right' />
-      <OnClickBtn className='btn' onBtnClick={() => console.log('save bookmark')} btnText='Bookmark' btnIcon='fas fa-bookmark' />
-      <section className="uploadContainer btn">
-        <input id='uploadedPdf' type='file' name='file' onChange={() => uploadPdf()} />
-        <p> Upload PDF <i className="fas fa-file-upload"></i></p>
+    <section className='options-bar'>
+      <section className="container">
+        <OnClickBtn className='btn sm' onBtnClick={() => previousPage()} btnText='' btnIcon='fas fa-lg fa-chevron-circle-left' />
+        <OnClickBtn className='btn sm' onBtnClick={() => nextPage()} btnText='' btnIcon='fas fa-lg fa-chevron-circle-right' />
+        <OnClickBtn className='btn' onBtnClick={() => console.log('save bookmark')} btnText='Bookmark' btnIcon='fas fa-bookmark' />
+        <section className="uploadContainer btn">
+          <input id='uploadedPdf' type='file' name='file' onChange={() => uploadPdf()} />
+          <p> Upload PDF <i className="fas fa-file-upload"></i></p>
+        </section>
+        <section className='btn btn-pg-search md'>
+          <input placeholder='Go To Page' onKeyDown={onPageSubmit} />
+        </section>
+        <OnClickBtn className='btn' onBtnClick={() => loadPdf('sample/sample.pdf')} btnText='Load Sample PDF' btnIcon='fas fa-file-pdf' />
       </section>
-      <section className='btn md'>
-        <input placeholder='Go To Page' onKeyDown={onPageSubmit} />
-      </section>
-      <OnClickBtn className='btn' onBtnClick={() => loadPdf('sample/sample.pdf')} btnText='Load Sample PDF' btnIcon='fas fa-file-pdf' />
     </section>
   )
 }
