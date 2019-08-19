392609a44b394a50e06d5a422f494f8e61a2322f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-19 11:04:34 -0500

    Updates

---
+++ precache-manifest.1e6aa3c844c513191c280157778d4294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1149ce292dd9a4404b6655dac9cd652c",
+    "url": "/pdf-viewer-reader/index.html"
+  },
+  {
+    "revision": "1199ba09fdde287ccd64",
+    "url": "/pdf-viewer-reader/static/css/main.c180925b.chunk.css"
+  },
+  {
+    "revision": "5fbda8677b423275f6d5",
+    "url": "/pdf-viewer-reader/static/js/2.398c3a69.chunk.js"
+  },
+  {
+    "revision": "1199ba09fdde287ccd64",
+    "url": "/pdf-viewer-reader/static/js/main.90412c79.chunk.js"
+  },
+  {
+    "revision": "de9e863dff476602dacb",
+    "url": "/pdf-viewer-reader/static/js/runtime~main.f8773d77.js"
+  }
+]);
